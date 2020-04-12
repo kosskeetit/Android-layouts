c52eb601a49c6562f654486a92cc8b5868fdf008
AntonyIS <antonyshikubu@gmail.com>
2020-04-12 22:26:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/layoutapp/LinearLayoutApp.kt
@@ -0,0 +1,12 @@
+package com.example.layoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutApp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout_app)
+    }
+}
